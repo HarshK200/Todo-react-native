ad385c021a4c5a56dc44dcfd01e4bb690a6aebdb
HarshK200 <harshkapadia04@gmail.com>
2025-05-05 21:28:10 +0530

    finished simple landing page

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
-import { Text, View, StyleSheet, ImageBackground } from "react-native";
+import { Text, Button, View, StyleSheet } from "react-native";
 import { useRouter } from "expo-router";
 import { BigButton } from "@/components/BigButton";
+import LandingPageIllustration from "@/assets/svg/landing_page_illustration";
 
 // Landing page
 export default function Index() {
@@ -11,46 +12,55 @@ export default function Index() {
 
   return (
     <View style={styles.container}>
-      <ImageBackground
-        style={styles.imageBackground}
-        source={require("@/assets/images/clean-purple-background.jpg")}
+      <LandingPageIllustration style={styles.illustration} />
+      <Text style={styles.headingText}>
+        The best app for managing your todos
+      </Text>
+      <BigButton
+        style={styles.bigButton}
+        textStyle={styles.bigButtonText}
+        title="Login"
+        onPress={() => router.navigate("/login")}
+      />
+      <Text
+        style={styles.smallButtonText}
+        onPress={() => console.log("navigate to signup page")}
       >
-        <Text style={styles.headingText}>
-          The best app for managing your todos
-        </Text>
-        <BigButton
-          style={styles.bigButton}
-          textStyle={styles.bigButtonText}
-          title="Login"
-          onPress={() => router.navigate("/login")}
-        />
-      </ImageBackground>
+        Don't have an account? create account
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  illustration: {
+    width: 300,
+    height: 500,
+    position: "absolute",
+    top: 250,
+  },
   container: {
     flex: 1,
-  },
-  imageBackground: {
-    flex: 1,
-    paddingVertical: 30,
     alignItems: "center",
+    position: "relative",
   },
   headingText: {
     paddingVertical: 120,
     paddingHorizontal: 40,
     fontSize: 60,
     fontWeight: "500",
-    color: "#a66ff5",
+    color: "#000",
   },
   bigButton: {
-    marginTop: 100,
+    marginTop: 150,
     borderRadius: 20,
     backgroundColor: "#4A3EFF",
   },
   bigButtonText: {
     color: "#fff",
+  },
+  smallButtonText: {
+    paddingVertical: 10,
+    fontSize: 16,
   },
 });
